474d3231ae650565f862e6c2ed865eb188fddc35
heminming <minming.he@hdtmedia.com>
2017-11-07 13:52:19 +0800

    更改RedisConfig

--- src/main/java/pers/com/config/RedisConfig.java
+++ src/main/java/pers/com/config/RedisConfig.java
@@ -14,51 +14,15 @@ public class RedisConfig {
 	@Autowired
 	private RedisConnectionFactory redisConnectionFactory;
 
-	/**
-	 * 实例化 RedisTemplate 对象
-	 * 
-	 * @return
-	 */
 	@Bean
 	public RedisTemplate<String, Object> functionDomainRedisTemplate() {
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-		this.initRedisTemplate(redisTemplate, redisConnectionFactory);
-		return redisTemplate;
-	}
-
-	/**
-	 * 序列化设置
-	 */
-	private void initRedisTemplate(RedisTemplate<String, Object> redisTemplate, RedisConnectionFactory factory) {
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
 		redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
 		redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
-		redisTemplate.setConnectionFactory(factory);
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+		return redisTemplate;
 	}
 
-	@Bean
-	public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate) {
-		return redisTemplate.opsForHash();
-	}
-
-	@Bean
-	public ValueOperations<String, Object> valueOperations(RedisTemplate<String, Object> redisTemplate) {
-		return redisTemplate.opsForValue();
-	}
-
-	@Bean
-	public ListOperations<String, Object> listOperations(RedisTemplate<String, Object> redisTemplate) {
-		return redisTemplate.opsForList();
-	}
-
-	@Bean
-	public SetOperations<String, Object> setOperations(RedisTemplate<String, Object> redisTemplate) {
-		return redisTemplate.opsForSet();
-	}
-
-	@Bean
-	public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate) {
-		return redisTemplate.opsForZSet();
-	}
 }
