55335b5c70509c1fd29ff95a173d5725ded244ca
heminming <minming.he@hdtmedia.com>
2017-11-14 15:48:53 +0800

    更换线程池

---
+++ src/main/java/pers/com/config/ThreadPoolConfig.java
@@ -0,0 +1,22 @@
+package pers.com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * Created by minming.he on 2017/11/14.
+ */
+@Configuration
+public class ThreadPoolConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor(){
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(300);
+        threadPoolTaskExecutor.setKeepAliveSeconds(30000);
+        threadPoolTaskExecutor.setMaxPoolSize(300);
+        threadPoolTaskExecutor.setQueueCapacity(2000);
+        return threadPoolTaskExecutor;
+    }
+}
